7fcaa1c2ceb1569e52d93af52c3446f0cd6d1a3d
mohd-Fardin-rahman <mohdfardin097@gmail.com>
2025-06-16 17:50:13 +0530

    chnages second

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -10,11 +10,8 @@ function App() {
     <>
       <div>
       
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite +efef React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
